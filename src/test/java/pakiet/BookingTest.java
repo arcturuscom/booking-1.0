6ddb6ad2e083eb6dee5076f3a2692484989335b2
arcturuscom <benyazidanass@gmail.com>
2017-01-22 21:33:51 +0100

    BOK-008
    Enhance Assertions

--- src/test/java/pakiet/BookingTest.java
+++ src/test/java/pakiet/BookingTest.java
@@ -1,6 +1,8 @@
 package pakiet;
 
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -53,16 +55,48 @@ public class BookingTest {
 		Assert.assertTrue(isLoginChecked2);
 		Assert.assertFalse(isLoginChecked);
 	}
-
+	
 	/**
-	 * test getEmail method
+	 * assert user registration
 	 */
 	@Test
-	public void testGetEmail() {
-		String email = booking.getEmail("01:256d5775-1b2d-4ecb-a22b-603024297771");
-		Assert.assertEquals(email, "pezhman1024@gmail.com");
+	public void testRegister(){
+		boolean registered = booking.register("benyazidanass@gmail.com", "77963b7a931377ad4ab5ad6a9cd718aa", "Klient");
+		Assert.assertTrue(registered);
 	}
-
+	
+	/**
+	 * Assert login usecase
+	 */
+	@Test
+	public void testLog(){
+		ResultSet rs = booking.log("ddd@ddd.pl", "77963b7a931377ad4ab5ad6a9cd718aa");
+		Assert.assertNotNull(rs);
+		try {
+			Assert.assertTrue(rs.next());
+		} catch (SQLException e) {
+			LOGGER.log(Level.SEVERE, e.getMessage(), e);
+		}
+	}
+	
+	/**
+	 * Assert add Event
+	 */
+	@Test
+	public void testAddEvent(){
+		boolean addEvent = booking.addEvent("01:12:2dsfe5", "event_1", "malmo", "place_1", new Timestamp(new Date().getTime()), 3, 5456);
+		Assert.assertTrue(addEvent);
+	}
+	
+	/**
+	 * Assert edit event
+	 */
+	@Test
+	public void testEditEvent(){
+		boolean editEvent = booking.editEvent("01:12", "edited_event", "Paris", "place_edited", new Timestamp(new Date().getTime()), 2, 500);
+		Assert.assertTrue(editEvent);
+	}
+	
 	/**
 	 * test get all events
 	 */
@@ -79,6 +113,94 @@ public class BookingTest {
 			}
 	
 	}
+	
+	@Test
+	public void testGetEventsByUserId(){
+		ResultSet rs = booking.getEvents("01:12");
+		Assert.assertNotNull(rs);
+		if (rs != null)
+			try {
+				Assert.assertFalse(rs.next());
+			} catch (SQLException e) {
+				LOGGER.log(Level.SEVERE, e.getMessage(), e);
+				e.printStackTrace();
+			}
+	}
+	
+	@Test
+	public void testGetEventById(){
+		ResultSet rs = booking.getEvent("01:01348b92-d3ae-4b53-abf3-5e84615d9b0d");
+		Assert.assertNotNull(rs);
+		if (rs != null)
+			try {
+				Assert.assertTrue(rs.next());
+			} catch (SQLException e) {
+				LOGGER.log(Level.SEVERE, e.getMessage(), e);
+				e.printStackTrace();
+			}
+	}
+
+	@Test
+	public void testBookTickets(){
+		boolean bookTicket = booking.bookTickets("01:01348b92-d3ae-4b53-abf3-5e84615d9b0d", "01:12", 5);
+		Assert.assertTrue(bookTicket);
+	}
+	
+	@Test
+	public void removeEvent(){
+		boolean eventRemoved = booking.removeEvent("01:12:2dsfe5");
+		Assert.assertTrue(eventRemoved);
+	}
+	
+	@Test
+	public void getReservationList(){
+		ResultSet rs = booking.getReservationList("01:12");
+		Assert.assertNotNull(rs);
+		if (rs != null)
+			try {
+				Assert.assertTrue(rs.next());
+			} catch (SQLException e) {
+				LOGGER.log(Level.SEVERE, e.getMessage(), e);
+				e.printStackTrace();
+			}
+	}
+	
+	@Test
+	public void testGetReservationsEvent() {
+		ResultSet rs = booking.getReservationsEvents("01:01348b92-d3ae-4b53-abf3-5e84615d9b0d");
+		Assert.assertNotNull(rs);
+		if (rs != null)
+			try {
+				Assert.assertTrue(rs.next());
+			} catch (SQLException e) {
+				LOGGER.log(Level.SEVERE, e.getMessage(), e);
+				e.printStackTrace();
+			}
+
+	}
+	
+	@Test
+	public void testGetReservations(){
+		ResultSet rs = booking.getReservations("01:15e77c37-8a14-4bde-ad5a-d0246ee322e8");
+		Assert.assertNotNull(rs);
+		if (rs != null)
+			try {
+				Assert.assertTrue(rs.next());
+			} catch (SQLException e) {
+				LOGGER.log(Level.SEVERE, e.getMessage(), e);
+				e.printStackTrace();
+			}
+	}
+	
+	/**
+	 * test getEmail method
+	 */
+	@Test
+	public void testGetEmail() {
+		String email = booking.getEmail("01:256d5775-1b2d-4ecb-a22b-603024297771");
+		Assert.assertEquals(email, "pezhman1024@gmail.com");
+	}
+
 
 	/**
 	 * test get news method
